8ede5080b802dd4676508bb7d56ae97c69359d7a
Harran Ali <harran.m@gmail.com>
2021-04-14 01:03:48 +0400

    implement move

--- localstorage/local.go
+++ localstorage/local.go
@@ -23,7 +23,7 @@ type FileInfo struct {
 	Size                 int64
 	Extension            string
 	Path                 string
-	FsFileInfo           fs.FileInfo
+	FsFileInfo           fs.FileInfo //golang's fs file info
 }
 
 var local *LocalStorage
@@ -59,7 +59,7 @@ func (l *LocalStorage) FileInfo(filepath string) (fileinfo FileInfo, err error) 
 	return fileinfo, nil
 }
 
-// Put files in the root directory,
+// Put files in the root directory from external locations,
 // filepath is the full path to the file you would like to put
 // it returns error incase there was
 func (l *LocalStorage) Put(filepath string) error {
@@ -109,7 +109,7 @@ func (l *LocalStorage) Put(filepath string) error {
 	return err
 }
 
-// PutAs  puts files in the root directory with the given name
+// PutAs  puts files in the root directory from external directory with the given name in the second parameter
 // the first arg 'filepath' is the file path
 // the second arg 'filename' is the name of the new file
 // filepath is the full path to the file you would like to put
@@ -161,22 +161,22 @@ func (l *LocalStorage) PutAs(filepath string, filename string) error {
 	return err
 }
 
-// Copy helps you copy files within the root directory
-// Please note that the refrence of the paths of these files is the root dir
+// Copy helps you copy files within the root folder
+// Please note that the refrence of the paths of these files is the root folder
 // it accepts the source file starting from the root folder
-// and the destination file starting from the root folder
+// and the destination folder starting from the root folder
 // it returns an error incase there any
-func (l *LocalStorage) Copy(srcfile string, destfile string) error {
+func (l *LocalStorage) Copy(srcfile string, destfolder string) error {
 	//unify slashes
 	srcfile = filepath.ToSlash(srcfile)
-	destfile = filepath.ToSlash(destfile)
+	destfolder = filepath.ToSlash(destfolder)
 
 	// construct the full paths
 	srcFileFullPath := filepath.Join(l.rootFolder, srcfile)
-	DestFileFullPath := filepath.Join(l.rootFolder, destfile)
+	DestFileFullPath := filepath.Join(l.rootFolder, destfolder)
 
-	// make sure the path of dest exists
-	os.MkdirAll(filepath.Dir(DestFileFullPath), 0755)
+	// make sure the path of dest folder exists
+	os.MkdirAll(DestFileFullPath, 0755)
 
 	// make sure the source file exists
 	s, err := os.Stat(srcFileFullPath)
@@ -188,7 +188,7 @@ func (l *LocalStorage) Copy(srcfile string, destfile string) error {
 	}
 
 	// make sure there is no file with the same name in destFile
-	_, err = os.Stat(DestFileFullPath)
+	_, err = os.Stat(path.Join(DestFileFullPath, s.Name()))
 	if !os.IsNotExist(err) {
 		return errors.New("the file is already exists")
 	}
@@ -201,7 +201,7 @@ func (l *LocalStorage) Copy(srcfile string, destfile string) error {
 	defer srcFile.Close()
 
 	// create a file in destination with the same name of source fil
-	destFile, err := os.Create(DestFileFullPath)
+	destFile, err := os.Create(path.Join(DestFileFullPath, s.Name()))
 	if err != nil {
 		return err
 	}
@@ -221,6 +221,73 @@ func (l *LocalStorage) Copy(srcfile string, destfile string) error {
 			return err
 		}
 	}
+	return err
+}
+
+// Move helps you Move files within the root folder
+// Please note that the refrence of the paths of these files is the root folder
+// it accepts the source file starting from the root folder
+// and the destination folder starting from the root folder
+// it returns an error incase there any
+func (l *LocalStorage) Move(srcfile string, destfolder string) error {
+	//unify slashes
+	srcfile = filepath.ToSlash(srcfile)
+	destfolder = filepath.ToSlash(destfolder)
+
+	// construct the full paths
+	srcFileFullPath := filepath.Join(l.rootFolder, srcfile)
+	DestFileFullPath := filepath.Join(l.rootFolder, destfolder)
+
+	// make sure the path of dest folder exists
+	os.MkdirAll(DestFileFullPath, 0755)
+
+	// make sure the source file exists
+	s, err := os.Stat(srcFileFullPath)
+	if os.IsNotExist(err) {
+		return err
+	}
+	if !s.Mode().IsRegular() {
+		return errors.New("File is not in regular mode")
+	}
+
+	// make sure there is no file with the same name in destFile
+	_, err = os.Stat(path.Join(DestFileFullPath, s.Name()))
+	if !os.IsNotExist(err) {
+		return errors.New("the file is already exists")
+	}
+
+	// open the source file
+	srcFile, err := os.Open(srcFileFullPath)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	// create a file in destination with the same name of source fil
+	destFile, err := os.Create(path.Join(DestFileFullPath, s.Name()))
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	// copy the file content
+	buf := make([]byte, 100)
+	for {
+		n, err := srcFile.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+		if _, err := destFile.Write(buf[:n]); err != nil {
+			return err
+		}
+	}
+
+	// remove the source file
+	srcFile.Close()
+	err = os.Remove(srcFileFullPath)
 	return err
 }
 
--- stowage.go
+++ stowage.go
@@ -24,7 +24,8 @@ type Disk interface {
 	FileInfo(filepath string) (fileinfo localstorage.FileInfo, err error)
 	Put(filepath string) error
 	PutAs(filepath string, filename string) error
-	Copy(srcfile string, destfile string) error
+	Copy(srcfile string, destfolder string) error
+	Move(srcfile string, destfile string) error
 }
 
 type Stowage struct {
